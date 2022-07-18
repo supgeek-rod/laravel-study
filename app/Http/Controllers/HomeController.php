801d6f6bdf5180462a059be6c8a7c268b7c5406c
SupeRodv2 <supgeek.rod@gmail.com>
2022-07-18 15:26:33 +0800

    debug: 调试 styleci

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,11 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
+        echo 1 + 2 + 3;
         echo 'test-styleci-rule' . 'binary_operator_spaces';
+
+        // echo 1+2+3;
+        // echo 'test-styleci-rule'.'binary_operator_spaces';
 
         dd();
     }
